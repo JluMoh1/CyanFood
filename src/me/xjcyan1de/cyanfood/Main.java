c8c4925841843f92229bacf2ad08586621e21fb6
kirill3345 <kirill3345@yandex.ru>
2017-02-28 18:11:39 +0300

    Update Main.java

--- src/me/xjcyan1de/cyanfood/Main.java
+++ src/me/xjcyan1de/cyanfood/Main.java
@@ -22,8 +22,9 @@ public class Main extends JavaPlugin {
 
     public ItemStack getSkull(MaterialData material, String texture) {
         try {
-            if (texture.equals("NO_SKULL_SPECIFIED")) return material.toItemStack(1);
-                return CustomSkull.getItem(texture);
+            if (texture.equals("NO_SKULL_SPECIFIED"))
+                return material.toItemStack(1);
+            return CustomSkull.getItem(texture);
         } catch (Exception ex) {
             return material.toItemStack(1);
         }
@@ -43,8 +44,7 @@ public class Main extends JavaPlugin {
             category_plants = new Category(new CustomItem(getSkull(Material.NETHER_STALK, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTVhNWM0YTBhMTZkYWJjOWIxZWM3MmZjODNlMjNhYzE1ZDAxOTdkZTYxYjEzOGJhYmNhN2M4YTI5YzgyMCJ9fX0="), "§7Растения и фрукты", "", "§a> Кликни, чтобы открыть"));
             registerItems();
         } catch (Exception ex) {
-            if (ex.getLocalizedMessage() != null)
-                server.broadcastMessage("Ошибка запуска CyanFood " + ex.getLocalizedMessage());
+            server.broadcastMessage("Ошибка запуска CyanFood " + ex.getLocalizedMessage());
         }
     }
 
